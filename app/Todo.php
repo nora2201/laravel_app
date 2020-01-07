8b77bfe918f81a7b58b2075c017443b74706a867
toripoppo <toripoppowork@gmail.com>
2020-01-07 21:21:14 +0900

    'ソフトデリート(倫理削除)機能の実装'

--- app/Todo.php
+++ app/Todo.php
@@ -3,16 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
-     protected $fillable = [
-       'title',
-       'user_id'
-     ];
   
-      public function getByUserId($id)
-      {
-        return $this->where('user_id', $id)->get();
-      }
+    use SoftDeletes;
+  
+    protected $fillable = [
+      'title',
+      'user_id'
+    ];
+  
+    public function getByUserId($id)
+    {
+      return $this->where('user_id', $id)->get();
+    }
+  
+    protected $dates = ['deleted_at'];
 }
